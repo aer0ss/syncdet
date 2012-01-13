bcd28eac53e0044ef6e0aa42b0344d9fe1022cf6
Mark Jeffrey <mark@aerofs.com>
2012-01-13 11:45:07 -0800

    Tarring the actor src instead of copying individually

--- syncdet/controller/deploy.py
+++ syncdet/controller/deploy.py
@@ -4,7 +4,7 @@
 # - to deploy test case source and scenarios to actors
 # - to collect log files from actors to the controller's system
 #############################################################################
-import os.path
+import os.path, os, tarfile
 
 import lib, log
 import config, systems
@@ -17,6 +17,7 @@ ACTOR_PY_FILES =     ( 'systems.py', 'config.py', 'systemsdef.py',
                        'case/syncdet_case_sync.py',
                        'case/__init__.py',
                       )
+ACTOR_TAR_PATH = 'actorsrc.tar.gz'
 
 # Deploys the "case/actor" package source code files
 # to all known Systems
@@ -24,22 +25,40 @@ ACTOR_PY_FILES =     ( 'systems.py', 'config.py', 'systemsdef.py',
 def deployActorSrc(verbose):
     if not config.DIRECTORY_SHARING:
         s_locRoot = lib.getLocalRoot()
-    
-        # Iterate over the number of systems
+
+        # Create a tarball of Actor source files
+        s_locTar = os.path.join(s_locRoot, ACTOR_TAR_PATH)
+        olddir = os.getcwd()
+        os.chdir(s_locRoot)
+        with tarfile.open(s_locTar, 'w:gz') as tar:
+            for f in ACTOR_PY_FILES:
+                assert os.path.exists(f)
+                tar.add(f)
+        os.chdir(olddir)
+
+        # Iterate over the systems, scp the tar file and extract it
         for system in systems.systems:
             assert isinstance(system, systems.System)
             s_dstRoot = system.detRoot
             
             # Ensure the destination DET root and case directories
             # are present on the remote machine.
-            cmd_mkdir = 'mkdir -p %s/case' % (s_dstRoot)
+            cmd_mkdir = 'mkdir -p {}'.format(s_dstRoot)
             system.executeRemoteCmd(os.P_WAIT, cmd_mkdir, verbose)
+
+            # Copy over the tar file
+            s_dstTar = os.path.join(s_dstRoot, ACTOR_TAR_PATH)
+            system.copyTo(s_locTar, s_dstTar)
+
+            # Extract the tar file to the syncdet root and remove the tar
+            cmd_extract = ('tar -xzf {0} -C {1}; '
+                           'rm {0};'
+                          ).format(s_dstTar, s_dstRoot)
+            system.executeRemoteCmd(os.P_WAIT, cmd_extract, verbose)
     
-            for f in ACTOR_PY_FILES:
-                s_locPath = os.path.join(s_locRoot, f)
-                assert os.path.exists(s_locPath)
-                s_dstPath = os.path.join(s_dstRoot, f)
-                system.copyTo(s_locPath, s_dstPath)
+
+        # Done with the tar file; locally remove it
+        if os.path.exists(s_locTar): os.remove(s_locTar)
 
 
 # Deploys the test case directory of source files to the specified system
