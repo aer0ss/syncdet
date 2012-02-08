d8fba842368bbae3bed69d835050fa8f8b3b84e7
Mark Jeffrey <mark@aerofs.com>
2012-02-08 11:38:26 -0800

    More documentation about aft directory

--- aft/lib/lib.py
+++ aft/lib/lib.py
@@ -4,10 +4,6 @@ import aerofs, file
 
 #######################################
 # configurations
-
-# a general timeout value
-WAIT_TIMEOUT    = 5
-
 aero = aerofs.createAeroFS()
 
 #######################################
@@ -29,18 +25,28 @@ def getFSRTRoot(): return aero.getRTRoot()
 #
 def getFSMountRoot(): return aero.getFSRoot()
 
+# Launch AeroFS daemon and cli 
+# (but CLI will not restart daemon on crash)
+#
 def launchFS(args = []): aero.launch(args)
+
+# Launch AeroFS daemon only
+#
 def launchFSDaemon(args = []): aero._launchDaemon(args)
 
+# Gracefully terminate AeroFS processes launched by this library
+#
 def terminateFS(ignoreError = False): aero.terminate()
 
+# Kill AeroFS processes launched by this library
+#
 def killFS(): aero.kill()
 
 
 # @param subsets     tuple of subsets of peers that can intra-communicate
 #
-def networkPartition(subsets): pass
-def clearNetworkPartition(): pass
+def networkPartition(subsets): raise NotImplementedError
+def clearNetworkPartition(): raise NotImplementedError
 
 # @param root        the directory to be the root of the tree
 # @param depth       0: make files in root, with no subdirectories
@@ -53,21 +59,23 @@ def makeDirTree(root, depth, nsubdirs, nfiles, maxfilesize = 100):
     return file.makeDirTree(dpath, depth, nsubdirs, nfiles, maxfilesize)
 
 
+#######################################################################
+# The following are experimental and not currently part of the API
 
 # @param content     must be one line only
 # @param mutable     true: return until the expected content appears
 #                    false: raise a case.Failure if the file content is not
 #                           empty and is not identical to what's expected
 #
-def waitFile(path, content, mutable = False):
-    pass
-
-def waitDir(path): 
-    while 1:
-        time.sleep(1)
-        if not os.path.exists(path): continue
-        if not os.path.isdir(path):
-            raise case.Failure, path + ' is not a directory'
-        break
-
-def waitDirTree(root, depth, nsubdirs, nfiles): pass
+#def waitFile(path, content, mutable = False):
+#    pass
+#
+#def waitDir(path): 
+#    while 1:
+#        time.sleep(1)
+#        if not os.path.exists(path): continue
+#        if not os.path.isdir(path):
+#            raise case.Failure, path + ' is not a directory'
+#        break
+#
+#def waitDirTree(root, depth, nsubdirs, nfiles): pass
