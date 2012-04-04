a6b4b1c867b8df6b800bb372d9cc614893e13d63
Mark Jeffrey <mark@aerofs.com>
2012-04-03 18:03:37 -0700

    Added a failure module similar to JUnit's Assert
    Also removed execute permissions on some python scripts.
    
    Change-Id: Iafba98855f3a86a07326ff73160927d8ae47d0a5

---
+++ deploy/syncdet/case/assertion.py
@@ -0,0 +1,28 @@
+'''
+This module provides methods to assert correct behaviour of a test.
+
+The methods are similar in signature to JUnit's Assert methods
+and Python's unittest.TestCase methods. If needed, extend this API
+with those methods in unittest.TestCase.
+'''
+
+class _Failure(Exception): pass
+
+def fail(msg = ''):
+    raise _Failure(msg)
+
+def assertTrue(cond, msg = 'condition must be true'):
+    if not cond: raise _Failure(msg)
+
+def assertFalse(cond, msg = 'condition must be false'):
+    assertTrue(not cond, msg)
+
+def assertEqual(expected, actual):
+    assertTrue(expected == actual,
+               ('{0} and {1} were expected to be equal, but are not'
+               ).format(str(expected), str(actual)))
+
+def assertNotEqual(first, second):
+    assertFalse(first == second,
+               ('{0} and {1} were expected to be not equal, but are'
+               ).format(str(first), str(second)))
