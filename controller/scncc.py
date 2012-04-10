5628dcd8928dab830043cdfba27640ffed6cf152
Adam Lesinski <adam@aerofs.com>
2012-04-10 14:06:42 -0700

    Print the name of an object
    - Helps with debugging

--- controller/scncc.py
+++ controller/scncc.py
@@ -16,6 +16,9 @@ class Object:
     def __init__(self, name):
         self.name = name
 
+    def __repr__(self):
+        return self.name
+
 class Unit:
     # contains Unit objects or Object objects
     children = []
