9bcadffd7f6f1e96ffb8c654b123b57fafa05362
Mark Jeffrey <mark@aerofs.com>
2012-04-23 16:10:32 -0700

    Updated sync.py to use underscore naming
    Also
    o new string formatting
    o updated documentation for sync.py
    o annotated where the documentation is out of date
    
    Change-Id: Ib73065da41e8c7a11d6585446a47df6673fd18ef

--- deploy/syncdet/case/sync.py
+++ deploy/syncdet/case/sync.py
@@ -21,26 +21,26 @@ def sync(id, waits = None, timeout = 0, voteYes = True):
 
     if voteYes: v = ''
     else:       v = 'DENY'
-    print "SYNC '%s' %s" % (str(id), v)
+    print "SYNC '{0}' {1}".format(id, v)
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((config.controllerAddress, param.SYNC_SERVICE_PORT))
-    data = buildRequest(id, waits, voteYes, timeout)
+    data = _build_request(id, waits, voteYes, timeout)
     s.send(data)
     # BUGBUG: we don't consider partial receiving now
     data = s.recv(4096)
-    return parseReply(id, data)
+    return _parse_reply(id, data)
 
 # pnid: this param and prev in conjunction identify a sync id.
-#       Therefore, syncPrev/Next on the local actor won't mess up with other
-#       actors with the same syncPrev/Next id.
+#       Therefore, sync_prev/next on the local actor won't mess up with other
+#       actors with the same sync_prev/next id.
 #
 # prev/next: the previous or next actor to sync. passing None will sync
 #       with the actor which actorId numerically precedes or succeed the local
 #       actor, and if the local actorId is zero or getSysCount() - 1, return OK
 #       immediately.
 #
-def syncPrev(pnid, prev = None, timeout = 0, voteYes = True):
+def sync_prev(pnid, prev = None, timeout = 0, voteYes = True):
 
     if prev == None:
         if case.getActorId() == 0: return case.OK
@@ -49,7 +49,7 @@ def syncPrev(pnid, prev = None, timeout = 0, voteYes = True):
     id = '{0}.{1}<=>{2}'.format(pnid, prev, case.getActorId())
     return sync(id, [prev], timeout, voteYes)
 
-def syncNext(pnid, next = None, timeout = 0, voteYes = True):
+def sync_next(pnid, next = None, timeout = 0, voteYes = True):
 
     if next == None:
         if case.getActorId() == case.getActorCount() - 1: return case.OK
@@ -58,36 +58,38 @@ def syncNext(pnid, next = None, timeout = 0, voteYes = True):
     id = '{0}.{1}<=>{2}'.format(pnid, case.getActorId(), next)
     return sync(id, [next], timeout, voteYes)
 
-def makeSyncId(id):
+def _make_sync_id(id):
     # convert spaces to underscores becuase we use the former as the delimiter
     syncId = string.translate(str(id), string.maketrans(' ', '_'))
-    return "%s.%s.%s" % (case.getModuleName(), case.getInstanceId(), syncId)
+    return "{0}.{1}.{2}".format(case.getModuleName(),
+                                case.getInstanceId(),
+                                syncId)
 
 #  request format:
 #    "len S module.sig.syncId actorId vote timeout actorId1,actorId2...actorIdN"
 # see controller/sync.py for detail
 #
-def buildRequest(id, waits, v, to):
+def _build_request(id, waits, v, to):
     if v: vote = 'y'
     else: vote = 'n'
     timeout = to
-    actorIds = '';
+    actorIds = ''
     for i in range(len(waits)):
         actorIds += str(waits[i])
         if i < len(waits) - 1: actorIds += ','
-    data = " S %s %s %s %d %s" % (makeSyncId(id), case.getActorId(),
-                                  vote, timeout, actorIds)
-    return "%d%s" % (len(data), data)
+    data = " S {0} {1} {2} {3} {4}".format(_make_sync_id(id), case.getActorId(),
+                                           vote, timeout, actorIds)
+    return "{0}{1}".format(len(data), data)
 
 # reply format
 #   "len module.sig.syncId result"
 # see controller/sync.py for detail
 #
-def parseReply(id, data):
+def _parse_reply(id, data):
     fields = string.split(data)
     assert len(fields) == 3
     assert int(fields[0]) == len(fields[1]) + len(fields[2]) + 2
-    assert fields[1] == makeSyncId(id)
+    assert fields[1] == _make_sync_id(id)
 
     if fields[2] == 'OK':
         return case.OK
--- examples/deploy/examples/synchronize.py
+++ examples/deploy/examples/synchronize.py
@@ -5,9 +5,9 @@ This test case demonstrates how to use sync primitives.
 from syncdet.case import sync
 
 def default():
-    sync.syncPrev(0)
+    sync.sync_prev(0)
     print 'the test machines should print this line one by one'
-    sync.syncNext(0)
+    sync.sync_next(0)
 
     sync.sync(0)
     print 'the test machines should print this line in parallel'
