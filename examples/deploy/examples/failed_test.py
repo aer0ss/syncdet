40d0416752b30038579813eae61a4d2cee0b6cde
Gregory Schlomoff <greg@aerofs.com>
2012-08-01 12:24:49 -0700

    Improving failure message

--- examples/deploy/examples/failed_test.py
+++ examples/deploy/examples/failed_test.py
@@ -4,5 +4,5 @@ This test case demonstrates how to fail a test.
 
 def fail():
     # raise any exception in the main thread to fail the test
-    raise RuntimeError('SyncDET is so fun')
+    raise RuntimeError('This test demoes a failed test. Failure is expected here.')
 spec = { 'default': fail }
