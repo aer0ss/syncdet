2c9efee4395910a9ff94eadb86cac5c6c772c890
Jonathan Gray <jonathan@aerofs.com>
2013-07-11 16:12:00 -0700

    remove hard-coded rtroot/anchor paths; use path from yaml instead

--- deploy/syncdet/case/tar_user_data.py
+++ deploy/syncdet/case/tar_user_data.py
@@ -7,16 +7,20 @@ def tar_user_data():
     print "Tarring up user_data"
     tf = tarfile.open('user_data-{0}.tar'.format(case.actor_id()), "w")
 
-    for d in case.extra_args():
-        ed=os.path.expanduser(d)
-        name=os.path.basename(d) + '-{0}'.format(case.actor_id())
+    actor = case.local_actor()
+
+    for attr in ['aero_rt_root', 'aero_root_anchor']:
+        d = getattr(actor, attr, None)
+        if d is None:
+            continue
+        ed = os.path.expanduser(d)
+        name = os.path.basename(d) + '-{0}'.format(case.actor_id())
         try:
             tf.add(ed, arcname=name)
             print "tar'd {0} as {1}".format(ed, name)
         except OSError as e:
             if e.errno == errno.ENOENT:
                 print "failed to tar {0} (not found)".format(ed)
-
 
     tf.close()
 
